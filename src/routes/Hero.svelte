5211fd5c0848c17c55df304d51c163cc35fe6184
VGowthamTheja <vempalligowthamtheja@gmail.com>
2023-02-10 16:27:50 +0530

    Initial commit

---
+++ src/routes/Hero.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<div class="hero">
+  <h2>Business Name or Tagline</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, ea voluptatum
+    dolore eum, totam rerum ullam similique perspiciatis suscipit aliquam,
+    adipisci quaerat omnis alias quisquam sequi corrupti voluptatem animi labore
+    consequuntur dicta accusamus vel nam dignissimos. A molestiae eligendi
+    inventore, laborum voluptatibus ea obcaecati? Magni enim odio dolore dolor
+    quos!
+  </p>
+</div>
+
+<style>
+    .hero {
+        background-color: #00ce22;
+        color: #fff;
+        padding: 5rem 0;
+    }
+    h2 {
+        margin: 0;
+        font-size: 3.5em;
+        font-weight: 500;
+    }
+    p {
+        color: #ddd;
+        font-size: 1.2em;
+        line-height: 1.6;
+    }
+</style>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,82 @@
+<script>
+  import { page } from "$app/stores";
+
+  const navs = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Services",
+      href: "/services",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ];
+
+  let currentRoute;
+  $: currentRoute = $page.route.id;
+</script>
+
+<nav>
+  <div class="container">
+    <h1>Start Bootstrap</h1>
+    <ul>
+      {#each navs as { title, href }}
+        <li>
+          <a {href} class:active={currentRoute === href} {title}>{title}</a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</nav>
+
+<style>
+  h1 {
+    margin: 0;
+    font-size: 1.2em;
+    font-weight: normal;
+  }
+  nav {
+    background-color: #212529;
+    color: #fff;
+  }
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+  }
+  ul {
+    list-style: none;
+    display: flex;
+    gap: 2rem;
+    padding: 0;
+    margin: 0;
+    margin-left: auto;
+    font-size: 0.98em;
+  }
+  li {
+    /* style to li uppercase text and bold and a gap */
+    text-transform: uppercase;
+    font-weight: normal;
+  }
+  a {
+    /* style to a text-decoration none and color */
+    text-decoration: none;
+    color: #ddd;
+  }
+  .active {
+    /* style to active class */
+    font-weight: bold;
+    color: #fff;
+    border-bottom: 4px solid #fff;
+    padding-bottom: 0.5em;
+  }
+</style>
